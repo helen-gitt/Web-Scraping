17ca5669f0c18478ca3e4780c82f1013099583e7
helen <helen@gplay.tech>
2024-11-22 18:23:11 +0200

    Update homework_4.py

--- stream-2/4/homework_4.py
+++ stream-2/4/homework_4.py
@@ -14,22 +14,23 @@ def get_content(url):
     except:
         response = requests.get(url)
         response.encoding = 'ISO-8859-1'  # замість 'utf-8'
-        # response.encoding = response.apparent_encoding
         print('request was sent')
         with open(name, 'w', encoding='utf-8') as f:
             f.write(response.text)
         return response.text
 
 # vacancy
-def extract_vacancy_titles(html_content):
+def extract_and_save_vacancy_titles(html_content, filename):
     pattern = r'<h3 class="jobCard_title m-0">(.*?)</h3>'
-    titles = re.findall(pattern, html_content, re.DOTALL)
-    return [html.unescape(title.strip()) for title in titles]
+    titles = [html.unescape(title.strip()) for title in re.findall(pattern, html_content, re.DOTALL)]
+    with open(filename, 'w', encoding='utf-8') as f:
+        for title in titles:
+            f.write(title + '\n')
+    print(f"Список вакансій збережено у файл: {filename}")
 
 
 if __name__ == '__main__':
     url = 'https://www.lejobadequat.com/emplois'
     html_content = get_content(url)
     if html_content:
-        vacancy_titles = extract_vacancy_titles(html_content)
-        print(vacancy_titles)
+        extract_and_save_vacancy_titles(html_content, 'vacancy_titles.txt')
